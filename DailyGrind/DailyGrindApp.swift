0f14c92c2855ebaca9082c9ad9bd19fd8fbff5b3
Joseph DeWeese <inclinedtactics@icloud.com>
2024-04-03 09:04:25 -0500

    Initial Commit

---
+++ DailyGrind/DailyGrindApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyGrindApp.swift
+//  DailyGrind
+//
+//  Created by Joseph DeWeese on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct DailyGrindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
